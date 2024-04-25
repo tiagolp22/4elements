7df62d0d4d32951ef99881a90c2a5adb9ade0e71
Clara Quintela <claraquintela@gmail.com>
2024-04-25 16:00:38 -0400

    Création de RecetteController

--- app/Http/Controllers/RecetteController.php
+++ app/Http/Controllers/RecetteController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Recette extends Controller
+class RecetteController extends Controller
 {
     /**
      * Display a listing of the resource.
