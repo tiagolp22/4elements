44e9c440bf9019a513b793b90cd03f8d79126a2d
Tiago Barros <tiagolp@msn.com>
2024-04-27 18:53:54 -0400

    make the shape with route

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -14,40 +14,45 @@
                         </h1>
                         <div class="w-full flex-1 mt-8">
                             <div class="mx-auto max-w-xs">
-                                <input
-                                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-orange-500 placeholder-orange-500 text-sm focus:outline-none focus:border-orange-500 focus:bg-white"
-                                    type="text" placeholder="Nom de la recette" />
-                                <input
-                                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-orange-500 placeholder-orange-500 text-sm focus:outline-none focus:border-orange-500 focus:bg-white mt-5"
-                                    type="text" placeholder="Courte description" />
-                                <textarea
-                                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-orange-500 placeholder-orange-500 text-sm focus:outline-none focus:border-orange-500 focus:bg-white mt-5"
-                                    placeholder="Liste des ingrédients" rows="4"></textarea>
-                                <textarea
-                                    class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-orange-500 placeholder-orange-500 text-sm focus:outline-none focus:border-orange-500 focus:bg-white mt-5"
-                                    placeholder="Étapes de préparation" rows="4"></textarea>
-                                <button
-                                    class="mt-5 tracking-wide font-semibold bg-orange-500 text-gray-100 w-full py-4 rounded-lg hover:bg-orange-600 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                                    <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                                        <circle cx="8.5" cy="7" r="4" />
-                                        <path d="M20 8v6M23 11h-6" />
-                                    </svg>
-                                    <span class="ml-3">
-                                        Enregistrer
-                                    </span>
-                                </button>
-                                <p class="mt-6 text-xs text-gray-600 text-center">
-                                    En cliquant sur le bouton d'enregistrement, vous acceptez nos
-                                    <a href="#" class="border-b border-gray-500 border-dotted">
-                                        Conditions d'utilisation
-                                    </a>
-                                    et notre
-                                    <a href="#" class="border-b border-gray-500 border-dotted">
-                                        Politique de confidentialité
-                                    </a>.
-                                </p>
+                                <form method="POST" action="{{ route('') }}">
+                                    @csrf
+                                    <input
+                                        class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-orange-500 placeholder-orange-500 text-sm focus:outline-none focus:border-orange-500 focus:bg-white"
+                                        type="text" placeholder="Nom de la Recette" name="nom" required />
+
+                                    <input
+                                        class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-orange-500 placeholder-orange-500 text-sm focus:outline-none focus:border-orange-500 focus:bg-white mt-5"
+                                        type="text" placeholder="Brève Description" name="description" required />
+
+                                    <textarea
+                                        class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-orange-500 placeholder-orange-500 text-sm focus:outline-none focus:border-orange-500 focus:bg-white mt-5"
+                                        placeholder="Liste des Ingrédients" name="ingredients" rows="4" required></textarea>
+
+                                    <textarea
+                                        class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-orange-500 placeholder-orange-500 text-sm focus:outline-none focus:border-orange-500 focus:bg-white mt-5"
+                                        placeholder="Étapes de Préparation" name="preparation" rows="4" required></textarea>
+
+                                    <button
+                                        class="mt-5 tracking-wide font-semibold bg-orange-500 text-gray-100 w-full py-4 rounded-lg hover:bg-orange-600 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                        <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round">
+                                            <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                                            <circle cx="8.5" cy="7" r="4" />
+                                            <path d="M20 8v6M23 11h-6" />
+                                        </svg>
+                                        <span class="ml-3">Enregistrer</span>
+                                    </button>
+
+                                    <p class="mt-6 text-xs text-gray-600 text-center">
+                                        En cliquant sur le bouton "Enregistrer", vous acceptez nos
+                                        <a href="#" class="border-b border-gray-500 border-dotted">Conditions
+                                            d'Utilisation</a>
+                                        et notre
+                                        <a href="#" class="border-b border-gray-500 border-dotted">Politique de
+                                            Confidentialité</a>.
+                                    </p>
+                                </form>
+
                             </div>
                         </div>
                     </div>
