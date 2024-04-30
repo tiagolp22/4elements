b02cadc851cd2fb85ef8031e0b8797bd659233d7
Clara Quintela <claraquintela@gmail.com>
2024-04-30 12:03:18 -0400

    Routes reglés

--- app/Http/Controllers/RecetteController.php
+++ app/Http/Controllers/RecetteController.php
@@ -22,7 +22,7 @@ class RecetteController extends Controller
      */
     public function create()
     {
-        //
+        return view("form");
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecetteController;
 
 
-Route::resource('/', RecetteController::class);
+Route::resource('/recettes', RecetteController::class);
+Route::get('/', [RecetteController::class, 'index']);
+
+// Route::get('/form', function () {
+
+//     return view('form');
+// })->name('form');
 
 
-Route::get('/form', function () {
-    return view('form');
-})->name('form');
-
-Route::get('/recettes/{recette}', [RecetteController::class, 'show'])->name('recettes.show');
-
-Route::post('/recettes/store', [RecetteController::class, 'store'])->name('recettes.store');
-
-Route::post('/form', 'RecetteController@store')->name('recette.store');
+// Route::post('/form',  [RecetteController::class, 'store'])->name('recette.store');
 
 
-Route::get('/recette', function () {
-    return view('recette');
-})->name('recette');
+// Route::get('/recette', function () {
+//     return view('recette');
+// })->name('recette');
 
-Route::get('/recettes/{recette}/edit', [RecetteController::class, 'edit'])->name('recettes.edit');
-Route::post('/recettes/{recette}/edit', [RecetteController::class, 'update'])->name('recettes.update');
+// Route::get('/recettes/{recette}/edit', [RecetteController::class, 'edit'])->name('recettes.edit');
+// Route::post('/recettes/{recette}/edit', [RecetteController::class, 'update'])->name('recettes.update');
+// Route::get('/recettes/{recette}', [RecetteController::class, 'show'])->name('recettes.show');
+// Route::post('/recettes/store', [RecetteController::class, 'store'])->name('recettes.store');
