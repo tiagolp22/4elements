6654fc0451e43f69cec89cfc0c5de341d6cf3331
Yasmin Rodrigues <yasminbresilienne@hotmail.com>
2024-04-30 13:55:44 -0400

    ajuster buttons page recette et changer style

--- resources/views/recette.blade.php
+++ resources/views/recette.blade.php
@@ -1,48 +1,55 @@
 @extends('partials.base')
 
-@section('title', '4 Élements')
+@section('title', 'Détails')
 
 @section('content')
 
-    <main>
+    <main class="bg-gray-100">
         <div class="py-8">
-            <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 bg-white p-10 shadow-lg">
                 <div class="flex flex-col md:flex-row -mx-4">
                     <div class="md:flex-1 px-4">
                         <div class="h-[460px] rounded-lg bg-orange-300 dark:bg-orange-700 mb-4">
                             <img class="w-full h-full object-cover" src="{{ Vite::asset('resources/img/imgForm.jpeg') }}"
                                 alt="Product Image">
                         </div>
-                        <div class="flex -mx-2 mb-4">
-                            <div class="w-1/2 px-2">
-                                <a href="{{ route('recettes.edit', $recette->id) }}"
-                                    class="w-full bg-orange-700 dark:bg-orange-600 text-white py-2 px-4 rounded-full font-bold hover:bg-orange-600 dark:hover:bg-orange-500">
-                                    Edit
-                                </a>
-                            </div>
-                            <div class="w-1/2 px-2">
-                                <form action="{{ route('recettes.destroy', ['recette' => $recette->id]) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit"
-                                        class="w-full bg-orange-200 dark:bg-orange-700 text-orange-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-orange-300 dark:hover:bg-orange-600">
-                                        Delete
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
                     </div>
 
                     <div class="md:flex-1 px-4">
                         <h2 class="text-2xl font-bold text-orange-800 mb-2"> {{ $recette->nom }}</h2>
-                        <p class="text-orange-600 text-sm mb-4">
+                        <h3 class="text-xl font-bold text-orange-800 mb-2 ">Ingredients:</h3>
+                        <p class=" text-sm mb-4">
                             {{ $recette->ingredients }}
                         </p>
                         <div>
-                            <span class="font-bold text-orange-700">Recette Description:</span>
-                            <p class="text-orange-600 text-sm mt-2">
+                            <span class="text-xl font-bold text-orange-800 mb-2 ">Recette Description:</span>
+                            <p class="text-sm mt-2">
+                                {{ $recette->description }}
+                            </p>
+                        </div>
+                        <div>
+                            <span class="text-xl font-bold text-orange-800 mb-2 ">Étapes:</span>
+                            <p class=" text-sm mt-2">
                                 {{ $recette->etapes }}
                             </p>
+                        </div>
+                        <div class="mt-4">
+                            <div class="px-2">
+                                <a href="{{ route('recettes.edit', $recette->id) }}"
+                                    class="w-full bg-orange-200 dark:bg-orange-700 text-orange-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-orange-300 dark:hover:bg-orange-600 block text-center">
+                                    Edit
+                                </a>
+                            </div>
+                            <div class="px-2 p-5">
+                                <form action="{{ route('recettes.destroy', ['recette' => $recette->id]) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"
+                                        class="w-full bg-orange-200 dark:bg-orange-700 text-orange-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-orange-300 dark:hover:bg-orange-600 block">
+                                        Delete
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
